723dc200d731c41a3764058d6dffd414128363d6
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-05-23 15:34:41 -0500

    feat: add pom dependencies and plugins for swaggger, jacoco and other tools}

---
+++ src/main/java/cvds/eci_salud_total_back/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package edu.eci.cvds.EciCredit.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5173",
+                        "http://localhost:3000" // <--- Agregar aquí la URL de tu frontend
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
