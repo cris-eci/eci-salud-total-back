945163858f8b9f866daa2143178efb82583d9774
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-05-23 15:39:04 -0500

    feat: add right corsconfig implementation

--- src/main/java/cvds/eci_salud_total_back/EciSaludTotalBackApplication.java
+++ src/main/java/cvds/eci_salud_total_back/EciSaludTotalBackApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EciSaludTotalBackApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EciSaludTotalBackApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EciSaludTotalBackApplication.class, args);
+    }
 }
--- src/main/java/cvds/eci_salud_total_back/config/CorsConfig.java
+++ src/main/java/cvds/eci_salud_total_back/config/CorsConfig.java
@@ -1,19 +1,18 @@
-package edu.eci.cvds.EciCredit.config;
+package cvds.eci_salud_total_back.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:5500",
                         "http://localhost:5173",
-                        "http://localhost:3000" // <--- Agregar aquí la URL de tu frontend
+                        "http://localhost:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
