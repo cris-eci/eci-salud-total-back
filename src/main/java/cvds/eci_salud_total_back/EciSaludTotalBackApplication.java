9cbbcd79aa71780564fd64ef9d1c9e00d50afad2
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-05-23 15:19:37 -0500

    chore: add initial scaffolding using spring initializr

---
+++ src/main/java/cvds/eci_salud_total_back/EciSaludTotalBackApplication.java
@@ -0,0 +1,13 @@
+package cvds.eci_salud_total_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciSaludTotalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciSaludTotalBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cvds/eci_salud_total_back/EciSaludTotalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cvds.eci_salud_total_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciSaludTotalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
