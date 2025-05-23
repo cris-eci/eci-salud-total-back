48a834ab4c4f81cd22413a2e1e4bf92b199bd3fa
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-05-23 16:06:29 -0500

    fix: cors problem credentials false

--- src/main/java/cvds/eci_salud_total_back/config/CorsConfig.java
+++ src/main/java/cvds/eci_salud_total_back/config/CorsConfig.java
@@ -16,6 +16,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
